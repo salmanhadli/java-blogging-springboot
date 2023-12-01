f1e44e62bd96fc459d9f9bf13c44172f6765f4bc
salmanhadli <skh174211@gmail.com>
2023-12-01 20:50:34 +0530

    first commit

---
+++ src/test/java/com/project/blogging/BloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.blogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
